39e001d2fd4f256ccb1894c3c5251007edda03b5
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-31 21:54:08 +0100

    Asynocronous and fetch delay

--- v8Engine.js
+++ v8Engine.js
@@ -1,1 +1,10 @@
-alert();
+let numberOne = 10;
+let numberTwo = 20;
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+let totalNumber = numberOne + numberTwo;
+
+console.log(totalNumber);
