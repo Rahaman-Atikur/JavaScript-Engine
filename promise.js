19938f2bd96c1ae430b80c8d80fee3e4de3d25aa
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-06-01 09:26:55 +0100

    Promise is running

--- promise.js
+++ promise.js
@@ -1,35 +1,12 @@
-let fetchData = () => {
-  return new Promise((resolve, reject) => {
-    let promiseStatus = true;
-    if(promiseStatus){
-        resolve({name:"Hero"})
+const fetchData =()=>{
+  return new Promise((resolve,reject)=>{
+    const status = true;
+    if(status){
+      resolve({name:"hero"});
     }
     else{
-        console.log("Server Error");
+      reject("server error");
     }
-  });
-};
-console.log(fetchData())
-
+  })
+}
 fetchData()
-.then(res=>console.log(res))
-.catch(err.console.log(err))
-
-const fetchData = () => {
-  return new Promise((resolve, reject) => {
-    let status = true;
-    if(status){
-        let mockData={json:()=>Promise.resolve({name:"hero"})
-    }
-    resolve(mockData);
-  }
-  else{
-    reject("Server Error");
-  }
-})
-}
-fetchData(){
-    .then(res =>res.json())
-    .then(data=>console.log(data))
-    .catch(err=>console.log(err))
-}
